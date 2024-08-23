020c0c81f03bc76fe932d7d018ddb4cecf208266
Bekhruz Salimov <bekhruzsalimovsobirovich@gmail.com>
2024-08-23 09:28:07 +0500

    update

--- app/Domain/Admin/Lessons/Requests/UpdateLessonRequest.php
+++ app/Domain/Admin/Lessons/Requests/UpdateLessonRequest.php
@@ -26,7 +26,7 @@ class UpdateLessonRequest extends FormRequest
             'course_plan_id' => 'required',
             'course_subject_id' => 'required',
             'date' => 'required',
-            'files.*' => 'sometimes|mimes:jpg,jpeg,png,pdf,docx,mp4,mpeg,avi,zip,rar|max:204800',
+            'files.*' => 'sometimes|max:204800',
             'lesson' => 'sometimes|json'
         ];
     }
