166027f628668a96c2b72b3583c1bee27cc2ddc9
Bekhruz Salimov <bekhruzsalimovsobirovich@gmail.com>
2024-08-23 08:26:23 +0500

    lesson resource add lesson id

--- app/Domain/Admin/Lessons/Resources/LessonResource.php
+++ app/Domain/Admin/Lessons/Resources/LessonResource.php
@@ -15,6 +15,7 @@ class LessonResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'course' => $this->course,
             'course_plan' => $this->course_plan,
             'course_subject' => $this->course_subject,
